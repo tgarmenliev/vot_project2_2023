a889e002c4dce4b67a61483d34dd7d85cae840bb
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2023-06-10 22:40:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,25 @@ function addPlan(event) {
   event.preventDefault();
 
   // Get form values
-  const description = document.getElementById('description').value;
-  const date = document.getElementById('date').value;
-  const hours = document.getElementById('hours').value;
+  const eventName = document.getElementById('eventName').value;
+  const eventDescription = document.getElementById('eventDescription').value;
+  const eventDate = document.getElementById('eventDate').value;
   const startLocation = document.getElementById('startLocation').value;
   const destination = document.getElementById('destination').value;
+  const departureTime = document.getElementById('departureTime').value;
+  const arrivalTime = document.getElementById('arrivalTime').value;
   const tripNumber = document.getElementById('tripNumber').value;
   const transport = document.getElementById('transport').value;
 
   // Create a new plan object
   const plan = {
-    description,
-    date,
-    hours,
+    eventName,
+    eventDescription,
+    eventDate,
     startLocation,
     destination,
+    departureTime,
+    arrivalTime,
     tripNumber,
     transport
   };
@@ -29,11 +33,13 @@ function addPlan(event) {
   plans.push(plan);
 
   // Clear form inputs
-  document.getElementById('description').value = '';
-  document.getElementById('date').value = '';
-  document.getElementById('hours').value = '';
+  document.getElementById('eventName').value = '';
+  document.getElementById('eventDescription').value = '';
+  document.getElementById('eventDate').value = '';
   document.getElementById('startLocation').value = '';
   document.getElementById('destination').value = '';
+  document.getElementById('departureTime').value = '';
+  document.getElementById('arrivalTime').value = '';
   document.getElementById('tripNumber').value = '';
 
   // Refresh the travel plans list
@@ -55,23 +61,31 @@ function displayPlans() {
     const planElement = document.createElement('div');
     planElement.className = 'travel-plan';
 
-    const description = document.createElement('p');
-    description.textContent = plan.description;
+    const eventName = document.createElement('p');
+    eventName.textContent = plan.eventName;
 
     const detailsButton = document.createElement('button');
     detailsButton.textContent = 'View Details';
+    detailsButton.className = 'details-button';
+    detailsButton.addEventListener('click', () => showDetails(index));
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', () => deletePlan(index));
 
-    planElement.appendChild(description);
+    planElement.appendChild(eventName);
     planElement.appendChild(detailsButton);
     planElement.appendChild(deleteButton);
     travelPlans.appendChild(planElement);
   });
 }
 
+// Function to show the details of a travel plan
+function showDetails(index) {
+  const plan = plans[index];
+  const details = `Event Name: ${plan.eventName}\nDescription: ${plan.eventDescription}\nDate: ${plan.eventDate}\nStart Location: ${plan.startLocation}\nDestination: ${plan.destination}\nDeparture Time: ${plan.departureTime}\nArrival Time: ${plan.arrivalTime}\nNumber of the Trip: ${plan.tripNumber}\nTransport: ${plan.transport}`;
+  alert(details);
+}
+
 // Add event listener to the form submission
 document.getElementById('travelForm').addEventListener('submit', addPlan);
-
